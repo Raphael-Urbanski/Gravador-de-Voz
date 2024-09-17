c58edb676c491d8629f316c830a1e42c69330228
anotação 0.0.1 <raphael.ralepher@gmail.com>
2024-09-16 23:06:54 -0300

    Definindo corpo do projeto

--- Gravador.py
+++ Gravador.py
@@ -1,1 +1,31 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import pyaudio
+import wave
+import threading
+import time
+import os
+import subprocess
 
+
+class Gravador(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+    def interface(self):
+        self.setWindowTitle("Amplay Gratuito")
+        self.setGeometry()
+
+    def design(self):
+        pass
+
+    def ajustar_tela(self):
+        pass
+
+    def gravar(self):
+        pass
+
+    def ver_pasta(self):
+        pass
